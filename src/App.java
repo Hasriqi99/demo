270f21b6a84935e92bc53bd9e6484e55f1b21ea1
Hasriqi99 <hasriq99@gmail.com>
2024-02-25 23:34:52 +0800

    First PR

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ public class App {
       String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
       for (String i : cars) {
         System.out.println(i);
-      }    
+      }
+      System.out.println("Hello World!");    
     }
   }
